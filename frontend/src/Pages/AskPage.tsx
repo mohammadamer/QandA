4a73bc595a29ccf81eb68da871aa2c62a1e39064
Mohammad Amer <mohammad.amer@atea.com>
2025-09-01 21:44:10 +0300

    Working with Forms

--- frontend/src/Pages/AskPage.tsx
+++ frontend/src/Pages/AskPage.tsx
@@ -1,5 +1,84 @@
 import React from 'react';
 import { Page } from '../PageTitle/Page';
-export const AskPage = () => (
-    <Page title="Ask a question">{null}</Page>
-);
+import { Fieldset, FieldContainer, FieldLabel, FieldInput, FieldTextArea, FormButtonContainer, PrimaryButton, FieldError, SubmissionSuccess } from '../Styles';
+import { useForm } from 'react-hook-form';
+import { postQuestion } from '../QuestionsData';
+
+type FormData = {
+    title: string;
+    content: string;
+};
+
+export const AskPage = () => {
+    const { register, formState: { errors }, handleSubmit, formState } = useForm<FormData>({ mode: 'onBlur' });
+    const [successfullySubmitted, setSuccessfullySubmitted] = React.useState(false);
+
+    const submitForm = async (data: FormData) => {
+        const result = await postQuestion({ title: data.title, content: data.content, userName: 'Fred', created: new Date() });
+        setSuccessfullySubmitted(!!result);
+    };
+
+    return (
+        <Page title="Ask a question">
+            <form onSubmit={handleSubmit(submitForm)}>
+                <Fieldset disabled={formState.isSubmitting || successfullySubmitted}>
+                    <FieldContainer>
+                        <FieldLabel htmlFor="title">
+                            Title
+                        </FieldLabel>
+                        <FieldInput
+                            {...register('title', { required: true, minLength: 10 })}
+                            id="title"
+                            name="title"
+                            type="text"
+                        />
+                        {errors.title && errors.title.type === 'required' && (
+                            <FieldError>
+                                You must enter the question title
+                            </FieldError>
+                        )}
+                        {errors.title && errors.title.type === 'minLength' && (
+                            <FieldError>
+                                The title must be at least 10 characters
+                            </FieldError>
+                        )}
+                    </FieldContainer>
+                    <FieldContainer>
+                        <FieldLabel htmlFor="content">
+                            Content
+                        </FieldLabel>
+                        <FieldTextArea
+                            {...register('content', { required: true, minLength: 50 })}
+                            id="content"
+                            name="content"
+                        />
+                        {errors.content && errors.content.type === 'required' && (
+                            <FieldError>
+                                You must enter the question content
+                            </FieldError>
+                        )}
+                        {errors.content && errors.content.type === 'minLength' && (
+                            <FieldError>
+                                The content must be at least 50 characters
+                            </FieldError>
+                        )}
+                    </FieldContainer>
+                    <FormButtonContainer>
+                        <PrimaryButton type="submit">
+                            Submit Your Question
+                        </PrimaryButton>
+                    </FormButtonContainer>
+                    {successfullySubmitted && (
+                        <SubmissionSuccess>
+                            Your question was successfully submitted
+                        </SubmissionSuccess>
+                    )}
+                </Fieldset>
+            </form>
+        </Page>
+    );
+}
+
+
+// Important note
+// More information on React Hook Form validation can be found at https://react-hook-form.com/get-started#Applyvalidation
--- frontend/src/Pages/QuestionPage.tsx
+++ frontend/src/Pages/QuestionPage.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Page } from '../PageTitle/Page';
 import { useParams } from 'react-router-dom';
-import { QuestionData, getQuestion } from '../QuestionsData';
+import { QuestionData, getQuestion, postAnswer } from '../QuestionsData';
 
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { gray3, gray6 } from '../Styles';
 import { AnswerList } from '../Answers/AnswerList';
+import { gray3, gray6, Fieldset, FieldContainer, FieldLabel, FieldTextArea, FormButtonContainer, PrimaryButton, FieldError, SubmissionSuccess } from '../Styles';
+import { useForm } from 'react-hook-form';
+
+
+type FormData = {
+    content: string;
+};
 
 export const QuestionPage = () => {
     const [question, setQuestion] = React.useState<QuestionData | null>(null);
+    const [successfullySubmitted, setSuccessfullySubmitted] = React.useState(
+        false,
+    );
+
     const { questionId } = useParams();
 
     React.useEffect(() => {
-        const doGetQuestion = async (
-            questionId: number) => {
+        const doGetQuestion = async (questionId: number) => {
             const foundQuestion = await getQuestion(questionId);
             setQuestion(foundQuestion);
         };
@@ -23,31 +32,76 @@ export const QuestionPage = () => {
         }
     }, [questionId]);
 
+    const { register, handleSubmit, formState } = useForm<FormData>({ mode: 'onBlur' });
+    const { errors } = formState;
+
+    const submitForm = async (data: FormData) => {
+        const result = await postAnswer({
+            questionId: question!.questionId,
+            content: data.content,
+            userName: 'Fred',
+            created: new Date(),
+        });
+        setSuccessfullySubmitted(result ? true : false);
+    };
+
     return (
         <Page>
-            <div css={css` background-color: white; padding: 15px 20px 20px 20px; border-radius: 4px; border: 1px solid ${gray6}; box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);`}>
-                <div css={css` font-size: 19px; font-weight: bold; margin: 10px 0px 5px;`}>
+            <div css={css`background-color: white;padding: 15px 20px 20px 20px;border-radius: 4px;border: 1px solid ${gray6};box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);`}>
+                <div css={css`font-size: 19px;font-weight: bold;margin: 10px 0px 5px;`}>
                     {question === null ? '' : question.title}
                 </div>
                 {question !== null && (
                     <React.Fragment>
-                        <p css={css` margin-top: 0px; background-color: white;`}>
+                        <p css={css`margin-top: 0px;background-color: white;`}>
                             {question.content}
                         </p>
-                        <div
-                            css={css` font-size: 12px; font-style: italic; color: ${gray3};`}>
+                        <div css={css` font-size: 12px;font-style: italic; color: ${gray3};`} >
                             {`Asked by ${question.userName} on
-                        ${question.created.toLocaleDateString()}
-                        ${question.created.toLocaleTimeString()}`}
+                            ${question.created.toLocaleDateString()} 
+                            ${question.created.toLocaleTimeString()}`}
                         </div>
                         <AnswerList data={question.answers} />
+                        <form onSubmit={handleSubmit(submitForm)} css={css`margin-top: 20px;`}>
+                            <Fieldset
+                                disabled={formState.isSubmitting || successfullySubmitted}
+                            >
+                                <FieldContainer>
+                                    <FieldLabel htmlFor="content">
+                                        Your Answer
+                                    </FieldLabel>
+                                    <FieldTextArea
+                                        {...register('content', { required: true, minLength: 50 })}
+                                        id="content"
+                                        name="content"
+                                    />
+                                    {errors.content && errors.content.type === 'required' && (
+                                        <FieldError>You must enter the answer</FieldError>
+                                    )}
+                                    {errors.content && errors.content.type === 'minLength' && (
+                                        <FieldError>
+                                            The answer must be at least 50 characters
+                                        </FieldError>
+                                    )}
+                                </FieldContainer>
+                                <FormButtonContainer>
+                                    <PrimaryButton type="submit">
+                                        Submit Your Answer
+                                    </PrimaryButton>
+                                </FormButtonContainer>
+                                {successfullySubmitted && (
+                                    <SubmissionSuccess>
+                                        Your answer was successfully submitted
+                                    </SubmissionSuccess>
+                                )}
+                            </Fieldset>
+                        </form>
                     </React.Fragment>
                 )}
             </div>
         </Page>
     );
 };
-
 
 // Important note
 // When using triple equals (===), we are checking for strict equality. This means
